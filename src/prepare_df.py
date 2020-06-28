06eb4dcaa21c084b0e388b15b6d97da4c13d8741
Ben Wetherfield <bwetherfield@gmail.com>
2020-06-28 15:59:24 -0700

    Implement dataframe prep

--- src/prepare_df.py
+++ src/prepare_df.py
@@ -4,30 +4,33 @@ import numpy as np
 from itertools import groupby
 from operator import add, itemgetter
 from functools import reduce
+import matplotlib.pyplot as plt
 
-def generate_row(musObject, part, pitchClass=np.nan):
+def generate_row(mus_object, part, pitch_class=np.nan):
     """
     Prepare rows for musical score `pandas.DataFrame`.
 
     Parameters
     ----------
-    musObject: music21.Music21Object
+    mus_object: music21.Music21Object
     part: music21.part
-    pitchClass: float
-        Default of `numpy.nan` for unpitched objects and a pitch class for rows with `Type` value `music21.pitch.Pitch`
+    pitch_class: float
+        Default of `numpy.nan` for unpitched objects and a pitch class for rows
+        with `Type` value `music21.pitch.Pitch`
 
     Returns
     -------
     dict
     """
     d = {}
-    d.update({'id': musObject.id,
+    d.update({'id': mus_object.id,
               'Part Name': part.partName,
-              'Offset': musObject.offset,
-              'Duration': musObject.duration.quarterLength,
-              'Type': type(musObject),
-              'Pitch Class': pitchClass})
+              'Offset': mus_object.offset,
+              'Duration': mus_object.duration.quarterLength,
+              'Type': type(mus_object),
+              'Pitch Class': pitch_class})
     return d
+
 
 def generate_df(score):
     """
@@ -45,7 +48,8 @@ def generate_df(score):
     parts = score.parts
     rows_list = []
     for part in parts:
-        for index, elt in enumerate(part.flat):
+        for index, elt in enumerate(part.flat.stripTies().getElementsByClass(
+                [note.Note, note.Rest, chord.Chord, bar.Barline])):
             if hasattr(elt, 'pitches'):
                 pitches = elt.pitches
                 for pitch in pitches:
@@ -54,9 +58,11 @@ def generate_df(score):
                 rows_list.append(generate_row(elt, part))
     return pd.DataFrame(rows_list)
 
+
 def combine_rests(df):
     """
-    Condense consecutive rests of a musical score (as stored in a `pandas.DataFrame`) into single long rests.
+    Condense consecutive rests of a musical score (as stored in a
+    `pandas.DataFrame`) into single long rests.
 
     Parameters
     ----------
@@ -66,18 +72,21 @@ def combine_rests(df):
     Returns
     -------
     pandas.DataFrame
-        `df` with consecutive rows with `Type` value `music21.note.Rest` combined, `Duration` values summed.
+        `df` with consecutive rows with `Type` value `music21.note.Rest`
+        combined, `Duration` values summed.
 
     """
     rests = df[df['Type'] == note.Rest]
 
-    # Group the indices of consecutive runs of rests that occur in the same part.
+    # Group the indices of consecutive runs of rests that occur in the same
+    # part
     parts_names = rests['Part Name'].unique()
     rest_runs_in_parts = []
     for part_name in parts_names:
         rest_idx = rests[rests['Part Name'] == part_name].index
         rest_runs_in_part = [list(map(itemgetter(1), g)) for _, g in
-                             groupby(enumerate(rest_idx), lambda ix: ix[1] - ix[0])]
+                             groupby(enumerate(rest_idx),
+                                     lambda ix: ix[1] - ix[0])]
         rest_runs_in_parts.append(rest_runs_in_part)
     rest_runs = reduce(add, rest_runs_in_parts)
 
@@ -93,3 +102,12 @@ def combine_rests(df):
     agg_func = dict.fromkeys(df, 'first')
     agg_func.update({'Duration': 'sum'})
     return df.groupby(get_initial_rest, axis=0).agg(agg_func)
+
+
+if __name__ == '__main__':
+    clara_search = corpus.search('clara')
+    clara_score = clara_search[0].parse()
+    clara_df = generate_df(clara_score)
+    clara_condensed_df = combine_rests(clara_df)
+    # clara_df.to_csv('test_dataframe_before_condensing.csv')
+    # clara_condensed_df.to_csv('test_dataframe_after_condensing.csv')
