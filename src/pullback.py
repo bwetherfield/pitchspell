3f4c4337d7346d19bf8cc38e606377c0b9183a40
Ben Wetherfield <bwetherfield@gmail.com>
2020-06-29 18:46:28 -0700

    Add docstrings for pullback definitions

--- src/pullback.py
+++ src/pullback.py
@@ -2,12 +2,42 @@ import numpy as np
 
 
 def pullback(f, shape, arr):
+    """
+    Returns a numpy array of the specified dimensions, defined by mapping
+    each index to an index of `arr` using `f`.
+
+    Parameters
+    ----------
+    f: Callable
+    shape: tuple
+    arr: numpy.ndarray
+
+    Returns
+    -------
+    numpy.ndarray
+
+    """
     idx = np.indices(shape)
     mapped_idx = f(idx)
     return arr(tuple(mapped_idx))
 
 
 def pullback(mapping, arr=None):
+    """
+    Returns a numpy array by mapping each index to its value in `mapping`,
+    using these values to index into `arr` (or an identity matrix with size
+    inferred if no array is supplied).
+
+    Parameters
+    ----------
+    mapping: Indexable (e.g. 1d numpy array)
+    arr: numpy.ndarray
+
+    Returns
+    -------
+    numpy.ndarray
+
+    """
     if arr is None:
         n = mapping.max() + 1
         arr = np.eye(n, dtype='int')
