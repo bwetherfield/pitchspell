a23df9e9ecb61c7d56b14f07e1dd234bd3886164
Ben Wetherfield <bwetherfield@gmail.com>
2020-07-12 12:54:43 -0700

    Add add_node position flexibility

--- pitchspell/prepare_edges.py
+++ pitchspell/prepare_edges.py
@@ -23,6 +23,13 @@ def concurrencies(starts, ends, size=None):
     return overlaps.astype('int') - np.eye(size, dtype='int')
 
 
-def add_node(arr, in_edges=0, out_edges=0):
-    output = np.insert(arr, arr.shape[0], out_edges, axis=0)
-    return np.insert(output, arr.shape[1], np.append(in_edges, 0), axis=1)
+def add_node(arr, row_pos=None, col_pos=None, in_edges=0, out_edges=0):
+    if row_pos is None and col_pos is not None:
+        row_pos = col_pos
+    elif col_pos is None and row_pos is not None:
+        col_pos = row_pos
+    elif row_pos is None and col_pos is None:
+        row_pos = arr.shape[0]
+        col_pos = arr.shape[1]
+    output = np.insert(arr, row_pos, out_edges, axis=0)
+    return np.insert(output, col_pos, np.append(in_edges, 0), axis=1)
