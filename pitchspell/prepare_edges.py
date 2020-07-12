b4234c70287c8a5712120f0337220dfd2f06ebad
Ben Wetherfield <bwetherfield@gmail.com>
2020-07-11 21:46:37 -0700

    Add skipping adjacencies generator

--- pitchspell/prepare_edges.py
+++ pitchspell/prepare_edges.py
@@ -1,4 +1,7 @@
 import numpy as np
 from .pullback import pullback
 
-np.array([2,3]) * np.eye(2)
+def skip_adjacency(hops, size):
+    indices = np.indices((size, size))
+    abs_differences = np.abs(indices[0] - indices[1])
+    return np.equal(abs_differences, hops).astype('int')
