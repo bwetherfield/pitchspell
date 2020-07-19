60da9f0ebee8191f2a46cffb43cdc99e5aa3e9dc
Ben Wetherfield <bwetherfield@gmail.com>
2020-07-19 15:03:20 -0700

    Add docstrings and adjust definition of add_node
    Assume input matrix is square

--- pitchspell/prepare_edges.py
+++ pitchspell/prepare_edges.py
@@ -2,12 +2,40 @@ import numpy as np
 
 
 def hop_adjacencies(hops, size):
+    """
+    Generate adjacency matrix where every nodes with indices that are `hops`
+    distance apart are connected.
+
+    Parameters
+    ----------
+    hops: int
+    size: int
+
+    Returns
+    -------
+    numpy.ndarray
+
+    """
     indices = np.indices((size, size))
     abs_differences = np.abs(indices[0] - indices[1])
     return np.equal(abs_differences, hops).astype('int')
 
 
 def concurrencies(starts, ends, size=None):
+    """
+    Generate adjacency array based on events that overlap, given their start
+    and end times.
+
+    Parameters
+    ----------
+    starts: numpy.ndarray
+    ends: numpy.ndarray
+    size: int or None
+
+    Returns
+    -------
+
+    """
     if size is None:
         size = len(starts)
     indices = np.indices((size, size))
@@ -23,13 +51,24 @@ def concurrencies(starts, ends, size=None):
     return overlaps.astype('int') - np.eye(size, dtype='int')
 
 
-def add_node(arr, row_pos=None, col_pos=None, in_edges=0, out_edges=0):
-    if row_pos is None and col_pos is not None:
-        row_pos = col_pos
-    elif col_pos is None and row_pos is not None:
-        col_pos = row_pos
-    elif row_pos is None and col_pos is None:
-        row_pos = arr.shape[0]
-        col_pos = arr.shape[1]
-    output = np.insert(arr, row_pos, out_edges, axis=0)
-    return np.insert(output, col_pos, np.append(in_edges, 0), axis=1)
+def add_node(arr, pos=None, in_edges=0, out_edges=0):
+    """
+    Add node to an adjacency matrix given its position (assumed at the end
+    of the list of nodes), its incident edges and its outgoing edges.
+
+    Parameters
+    ----------
+    arr: numpy.ndarray
+    pos: int
+    in_edges: int or numpy.ndarray
+    out_edges: int or numpy.ndarray
+
+    Returns
+    -------
+    numpy.ndarray
+
+    """
+    if pos is None:
+        pos = arr.shape[0]
+    output = np.insert(arr, pos, out_edges, axis=0)
+    return np.insert(output, pos, np.append(in_edges, 0), axis=1)
