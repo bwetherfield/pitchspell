770e6ba3d2ed59c1f0367b144dc29297f635e672
Ben Wetherfield <bwetherfield@gmail.com>
2020-07-19 14:55:38 -0700

    Add function to generate an edge indicator for minimum cut edges

---
+++ pitchspell/minimum_cut.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+def generate_complete_cut(arr):
+    """
+    Return matrix that is 1 at (i,j) only if arr[i] == 1 and arr[0] == 0.
+
+    Parameters
+    ----------
+    arr: numpy.ndarray
+
+    Returns
+    -------
+    numpy.ndarray
+
+    """
+    N = len(arr)
+    indices = np.indices((N, N), sparse=True)
+    return np.clip(arr[indices[1]] - arr[indices[0]], 0, 1)
