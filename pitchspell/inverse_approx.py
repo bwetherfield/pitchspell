fd4657040a7e0a0893d5655ecf7860c498453349
Ben Wetherfield <bwetherfield@gmail.com>
2020-11-08 13:38:10 +0000

    Refactor operations common to inverse and forward solving direction

--- pitchspell/inverse_approx.py
+++ pitchspell/inverse_approx.py
@@ -115,7 +115,6 @@ class ApproximateInverter(BaseEstimator):
         self.between_part_scalar = between_part_scalar
         self.pre_calculated_weights = pre_calculated_weights
 
-    # TODO: finish implementation
     def fit(self, X, y):
         """
         Constructs matrix inputs for scipy.optimize.linprog from `X` and `y`
@@ -129,103 +128,37 @@ class ApproximateInverter(BaseEstimator):
         y: numpy.ndarray (1D)
 
         """
-        N = len(y)
-        n_events = X[:, 0].max() + 1
-
-        part_adj = pullback(X[:, 2])
-        chain_adj = pullback(X[:, 1]) * part_adj
-        not_part_adj = ~part_adj
-
-        within_chain_adjs = list(map(
-            lambda arr: pullback(X[:, 0]) * chain_adj,
-            [hop_adjacencies(i, n_events) for i in range(self.distance_cutoff)]
-        ))
-        # Remove adjacency within the same note (between notes in the same
-        # event is fine)
-        within_chain_adjs[0] *= ~f_inverse(
-            lambda x: x // 2, (N, N), np.eye(
-                N // 2, dtype='int')
-        )
-
-        # Connect concurrent notes in different parts
-        between_part_adj = concurrencies(
-            X[:, 4], X[:, 5]
-        ) * not_part_adj
-
-        # Construct 'big M' edges between second and first subindex of each
-        # note in the score.
-        internal_adj = np.tile([[0, 0], [1, 0]], [N // 2, N // 2]) * np.repeat(
-            np.repeat(
-                np.eye(N // 2, N // 2, dtype='int'),
-                [2],
-                axis=1),
-            [2],
-            axis=0)
-        big_M = np.inf * internal_adj
-        big_M[np.isnan(big_M)] = 0
-        big_M = add_node(big_M)
-        big_M = add_node(big_M)
-
-        # Add a scale factor according to position in the score - present in
-        # the input matrix 'X'
-        idx = np.indices((N, N), sparse=True)
-        timefactor = X[:, -1][idx[0]] * X[:, -1][idx[1]]
-        timefactor = add_node(timefactor, out_edges=X[:, -1])
-        timefactor = add_node(timefactor, in_edges=X[:, -1])
-
-        # ----------------------------------------
-        # AUXILIARY MATRICES
-        # Generate full adjacency matrix, cut edges
-        source_adj = np.zeros((N,), dtype='int')
-        sink_adj = np.zeros((N,), dtype='int')
-        idx = np.indices((N // 2,), dtype='int') * 2
-        source_adj[idx] = 1
-        sink_adj[idx + 1] = 1
-        full_adj = sum(within_chain_adjs) + between_part_adj + internal_adj
-        full_adj = add_node(full_adj, out_edges=source_adj)
-        full_adj = add_node(full_adj, in_edges=sink_adj)
-        y_plus_source_sink = np.concatenate([y, [0, 1]])
-        cut = generate_complete_cut(y_plus_source_sink) * full_adj
-
-        # Adjacency with relative weights based on proximity in score
-        weighted_adj = sum([
-            pow(self.distance_rolloff, i) * adj for i, adj in
-            enumerate(within_chain_adjs)
-        ]) + self.between_part_scalar * between_part_adj
-        # add edges for source and sink edges
-        weighted_adj = add_node(weighted_adj, out_edges=source_adj)
-        weighted_adj = add_node(weighted_adj, in_edges=sink_adj)
-        combined_adj = weighted_adj * timefactor
+        N = X.shape[0]
+        adj, weighted_adj = self.extract_adjacencies(N, X)
 
         # ----------------------------------------
         # EQUALITY CONSTRAINTS
         # sum_(i=1)^n f_(i,k) - sum_(j=1)^n f_(k,j) = 0 for all k != s,t
-        flow_conditions = full_adj - full_adj.T
+        flow_conditions = adj - adj.T
         # Remove flow conditions from source and sink edges
         flow_conditions[-2:] = 0
         flow_conditions[:, -2:] = 0
+        # RHS
+        flow_conditions_rhs = np.zeros((N), dtype=int)
 
         # sum_(i=1)^n f_s,i - sum_(e in cut) c_e = delta
+        cut = self.generate_cut(adj, y)
         duality_constraint = np.concatenate([
             np.ones(N, dtype='int'), ~cut.flatten(), [1]
         ])
+        # RHS
+        duality_constraint_rhs = [0]
 
         if self.pre_calculated_weights:
             # c_i,j = t(i,j) * w_(p(i), p(j))
-            idx = np.indices((N // 2,), dtype='int') * 2
-            source_edges = self.source_edge_scheme[X[:, 3]]
-            source_edges[idx + 1] = 0
-            sink_edges = self.sink_edge_scheme[X[:, 3]]
-            sink_edges[idx] = 0
-            edge_weights = self.internal_scheme[
-                (X[:, 3] * 2 + np.indices((N,), dtype='int') % 2)[
-                    np.indices((N, N), dtype='int')
-                ]
-            ]
-            edge_weights = add_node(edge_weights, out_edges=source_edges)
-            edge_weights = add_node(edge_weights, in_edges=sink_edges)
             capacities_def_with_weights_given = np.eye(pow(N + 2, 2),
                                                        dtype='int')
+            edge_weights = self.get_weight_scalers(N, X)
+            big_M = self.get_big_M_edges(N)
+            # RHS
+            capacities_def_rhs = (
+                    weighted_adj * edge_weights + big_M
+            ).flatten()
         else:
             # c_i,j - t(i,j) * w_(p(i), p(j)) = 0
             pitch_class_indexing = X[:, 3] * 2 + np.indices((N,),
@@ -238,10 +171,12 @@ class ApproximateInverter(BaseEstimator):
             pitch_based_capacity[
                 np.indices((pow(N + 2, 2),)),
                 pc_edge_1d_idx
-            ] = combined_adj.flatten()
+            ] = weighted_adj.flatten()
             capacity_def_with_weights_variable = np.concatenate([
                 np.eye(pow(N + 2, 2)), ~pitch_based_capacity
             ], axis=1)
+            # RHS
+            capacities_def_rhs = np.zeros((pow(N + 2, 2)), dtype=int)
 
         # ----------------------------------------
         # INEQUALITY CONSTRAINTS
@@ -262,7 +197,6 @@ class ApproximateInverter(BaseEstimator):
             flow_conditions,
             np.indices(flow_conditions.shape)
         )
-        flow_conditions_rhs = np.zeros((N), dtype=int)
 
         duality_constraint_spaced = pad(
             (2 * pow(N + 2, 2) + 1 + pow(26, 2),),
@@ -273,7 +207,6 @@ class ApproximateInverter(BaseEstimator):
                 [-1]
             ])
         )
-        duality_constraint_rhs = [0]
 
         if self.pre_calculated_weights:
             abs_idx = np.indices((pow(N + 2, 2), pow(N + 2, 2)))
@@ -283,9 +216,6 @@ class ApproximateInverter(BaseEstimator):
                 capacities_def_with_weights_given,
                 abs_idx
             )
-            capacities_def_rhs = (
-                    combined_adj * edge_weights + big_M
-            ).flatten()
         else:
             pitch_based_capacity_abs_idx = np.indices(
                 capacity_def_with_weights_variable.shape
@@ -305,7 +235,6 @@ class ApproximateInverter(BaseEstimator):
                 capacity_def_with_weights_variable,
                 capacity_definition_spaced_idx
             )
-            capacities_def_rhs = np.zeros((pow(N + 2, 2)), dtype=int)
 
         # ----------------------------------------
         # SPACED INEQUALITY CONSTRAINTS
@@ -372,8 +301,143 @@ class ApproximateInverter(BaseEstimator):
                 ].flatten()
             ]
 
+    def get_weight_scalers(self, N, X):
+        """
+        Get weight scalers based on the pitches of the notes in the dataset X.
+
+        Parameters
+        ----------
+        N: int
+        X: numpy.ndarray
+
+        Returns
+        -------
+        numpy.ndarray
+
+        """
+        idx = np.indices((N // 2,), dtype='int') * 2
+        source_edges = self.source_edge_scheme[X[:, 3]]
+        source_edges[idx + 1] = 0
+        sink_edges = self.sink_edge_scheme[X[:, 3]]
+        sink_edges[idx] = 0
+        edge_weights = self.internal_scheme[
+            tuple(
+                (X[:, 3] * 2 + np.indices((N,), dtype='int')[0] % 2)[
+                    np.indices((N, N), dtype='int')
+                ]
+            )
+        ]
+        edge_weights = add_node(edge_weights, out_edges=source_edges)
+        edge_weights = add_node(edge_weights, in_edges=sink_edges)
+
+        return edge_weights
+
+    def get_big_M_edges(self, N):
+        """
+        Output the "big M" component graph which connects the up node and the
+        down node corresponding to a single note in the musical score by a
+        directed infinite weight edge.
+
+        Parameters
+        ----------
+        N: int
+
+        Returns
+        -------
+        numpy.ndarray
+
+        """
+
+        internal_adj = np.tile([[0, 0], [1, 0]], [N // 2, N // 2]) * np.repeat(
+            np.repeat(
+                np.eye(N // 2, N // 2, dtype='int'),
+                [2],
+                axis=1),
+            [2],
+            axis=0)
+        big_M = np.inf * internal_adj
+        big_M[np.isnan(big_M)] = 0
+        big_M = add_node(big_M)
+        big_M = add_node(big_M)
+
+        return big_M
+
+    def extract_adjacencies(self, N, X):
+        """
+        Generate unweighted and weighted adjacency structures from node data X.
+
+        Parameters
+        ----------
+        N: int
+        X: numpy.ndarray
+
+        Returns
+        -------
+        numpy.ndarray, numpy.ndarray
+
+        """
+        n_events = X[:, 0].max() + 1
+        part_adj = pullback(X[:, 2])
+        chain_adj = pullback(X[:, 1]) * part_adj
+        not_part_adj = ~part_adj
+        within_chain_adjs = list(map(
+            lambda arr: pullback(X[:, 0]) * chain_adj,
+            [hop_adjacencies(i, n_events) for i in range(self.distance_cutoff)]
+        ))
+
+        # Remove adjacency within the same note (between notes in the same
+        # event is fine)
+        within_chain_adjs[0] *= ~f_inverse(
+            lambda x: x // 2, (N, N), np.eye(
+                N // 2, dtype='int')
+        )
+
+        # Connect concurrent notes in different parts
+        between_part_adj = concurrencies(
+            X[:, 4], X[:, 5]
+        ) * not_part_adj
+
+        # Add a scale factor according to position in the score - present in
+        # the input matrix 'X'
+        idx = np.indices((N, N), sparse=True)
+        timefactor = X[:, -1][idx[0]] * X[:, -1][idx[1]]
+        timefactor = add_node(timefactor, out_edges=X[:, -1])
+        timefactor = add_node(timefactor, in_edges=X[:, -1])
+
+        # Generate adjacency matrix
+        source_adj = np.zeros((N,), dtype='int')
+        sink_adj = np.zeros((N,), dtype='int')
+        idx = np.indices((N // 2,), dtype='int') * 2
+        source_adj[idx] = 1
+        sink_adj[idx + 1] = 1
+        internal_adj = np.tile([[0, 0], [1, 0]], [N // 2, N // 2]) * np.repeat(
+            np.repeat(
+                np.eye(N // 2, N // 2, dtype='int'),
+                [2],
+                axis=1),
+            [2],
+            axis=0)
+        adj = sum(within_chain_adjs) + between_part_adj + internal_adj
+        adj = add_node(adj, out_edges=source_adj)
+        adj = add_node(adj, in_edges=sink_adj)
+
+        # Adjacency with relative weights based on proximity in score
+        weighted_adj = sum([
+            pow(self.distance_rolloff, i) * adj for i, adj in
+            enumerate(within_chain_adjs)
+        ]) + self.between_part_scalar * between_part_adj
+        weighted_adj = add_node(weighted_adj, out_edges=source_adj)
+        weighted_adj = add_node(weighted_adj, in_edges=sink_adj)
+        weighted_adj *= timefactor
+        return adj, weighted_adj
+
+    def generate_cut(self, adj, y):
+        y_plus_source_sink = np.concatenate([y, [0, 1]])
+        cut = generate_complete_cut(y_plus_source_sink) * adj
+        return cut
+
     def score(self, X, y=None):
         return self._score
 
     def predict(self, X):
-        return X[1]
+        adj, weighted_adj = self.extract_adjacencies(X)
